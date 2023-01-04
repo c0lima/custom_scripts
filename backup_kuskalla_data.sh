13d9f554e110ed53a6e111d3f70c8f2c920c771a
c0lima <colima.msc@gmail.com>
2023-01-04 15:47:58 -0300

    Add Kuskalla Backup

---
+++ backup_kuskalla_data.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Path to local backup directory
+LOCAL_DATA_DIR="/home/factoria/Desktop/plataforma-kuskalla/data"
+LOCAL_BACKUP_DIR="/home/factoria/.backup/kuskalla"
+DATE_FORMAT=$(date +"%Y-%m-%d")
+REMOTE_DRIVE_FOLDER="13ZV2FKtPBcGpBmRqqVShzkURLIoGj2a-" #server-db-backups/kuskalla
+LOCAL_DIR=${LOCAL_BACKUP_DIR}/${DATE_FORMAT}
+
+
+7z a ${LOCAL_BACKUP_DIR}/${DATE_FORMAT}.zip ../../.${LOCAL_DATA_DIR}
+gdrive upload --parent ${REMOTE_DRIVE_FOLDER} ${LOCAL_BACKUP_DIR}/${DATE_FORMAT}.zip
+
